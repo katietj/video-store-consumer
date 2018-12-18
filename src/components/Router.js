90854b86a6b9f2bac2717e5f23ab57a2c8a719bf
Alice Hsiao <alice.hsiao87@gmail.com>
2018-12-18 12:46:05 -0800

    Added navigation via React Router

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,21 +1,48 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import App from '../App';
 import Customers from './Customers';
 import Library from './Library';
 import Search from './Search';
+import NotFound from './NotFound';
 
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={ App } />
-                <Route path="/search" component={ Search } />
-                <Route path="/library" component={ Library } />
-                <Route path="/customers" component={ Customers } />
-            </Switch>
-        </BrowserRouter>
-    )
+    return <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/search">Search</Link>
+              </li>
+              <li>
+                <Link to="/library">Library</Link>
+              </li>
+              <li>
+                <Link to="/customers">Customers</Link>
+              </li>
+              <li>
+                <button>Selected Movie</button>
+              </li>
+              <li>
+                <button>Selected Customer</button>
+              </li>
+              <li>
+                <button>Check Out</button>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/search" component={Search} />
+            <Route path="/library" component={Library} />
+            <Route path="/customers" component={Customers} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </BrowserRouter>;
 }
 
 export default Router;
